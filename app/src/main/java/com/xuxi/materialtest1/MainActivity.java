7c9b44b5df7f1193e6d2a652bc26a96ba527e7a5
zqzal <936952020@qq.com>
2019-08-23 16:20:27 +0800

    toolbar add

--- app/src/main/java/com/xuxi/materialtest1/MainActivity.java
+++ app/src/main/java/com/xuxi/materialtest1/MainActivity.java
@@ -1,8 +1,12 @@
 package com.xuxi.materialtest1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.backup:
+                Toast.makeText(this,"You clicked Backup",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.delete:
+                Toast.makeText(this,"You clicked delete",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.settings:
+                Toast.makeText(this,"You cliced settings",Toast.LENGTH_SHORT).show();
+                break;
+                default:
+                    break;
+        }
+
+        return true;
     }
 }
