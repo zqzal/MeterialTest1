fd9cbb8dad48c833a692a2ae56dd165fc1637698
zqzal <936952020@qq.com>
2019-08-25 15:55:27 +0800

    add navgationview

--- app/src/main/java/com/xuxi/materialtest1/MainActivity.java
+++ app/src/main/java/com/xuxi/materialtest1/MainActivity.java
@@ -1,14 +1,23 @@
 package com.xuxi.materialtest1;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLaout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +26,24 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        mDrawerLaout = findViewById(R.id.drawer_layout);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        }
+
+        NavigationView navView = findViewById(R.id.nav_view);
+//        navView.setCheckedItem(R.id.nav_call);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                mDrawerLaout.closeDrawers();
+                return true;
+            }
+        });
 
     }
 
@@ -38,6 +65,9 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case R.id.settings:
                 Toast.makeText(this,"You cliced settings",Toast.LENGTH_SHORT).show();
+                break;
+            case android.R.id.home:
+                mDrawerLaout.openDrawer(GravityCompat.START);
                 break;
                 default:
                     break;
